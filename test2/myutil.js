75e92c95125a2e9327376de75647d6742e22b35a
cozitrip <pwu@cozitrip.com>
2019-11-02 13:00:16 +1100

    added simple module

---
+++ test2/myutil.js
@@ -0,0 +1,7 @@
+var myadd = {
+    add: function(a, b) { 
+        return a + b;
+    }    
+};
+
+module.exports = myadd
---
+++ test2/testModule.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let log = require('../log');
+let myadd = require('./myutil');
+
+log.info('3+5=' + myadd.add(3,5));
