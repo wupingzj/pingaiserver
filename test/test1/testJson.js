262e7d0a87ff7ee874d4499ca4f6a20490cb5b91
cozitrip <pwu@cozitrip.com>
2019-11-04 12:43:05 +1100

    JSON works

---
+++ test/test1/testJson.js
@@ -0,0 +1,5 @@
+
+const str =  new Date(2006, 0, 2, 15, 4, 5);
+
+let s = JSON.stringify(str);
+console.log("json=" + s);
---
+++ utils/secrets.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const secrets = {
+    MONGODB_URI: "mongodb://localhost:27017/smartbot",
+    SESSION_SECRET: "mysecret"
+    
+    // add: (a, b) => { 
+    //     return a + b;
+    // },
+
+    // minus: (a, b) => { 
+    //     return a - b;
+    // }
+};
+
+
+module.exports = secrets
