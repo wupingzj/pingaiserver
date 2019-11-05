1aff6115f13f263f35b4109856ce379a1820b2d3
Kevin Wu <kevinpingsydney@gmail.com>
2019-11-05 13:56:06 +1100

    added User document

---
+++ test/test2/myutil2.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// const myadd = {
+//     add: function(a, b) { 
+//         return a + b;
+//     }    
+// };
+
+module.exports.myutil = {
+    add: (a, b) => { 
+        return a + b;
+    },
+
+    minus: (a, b) => { 
+        return a - b;
+    }
+};
+
+
+
